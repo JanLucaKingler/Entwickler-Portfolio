42828c29c38fbf2735945ab929cee23109eb4eca
kingler <janlucakingler@gmail.com>
2025-08-20 15:06:08 +0200

    add transporting message to docx text

--- portfolio/scripts/contact/contact.js
+++ portfolio/scripts/contact/contact.js
@@ -1,10 +1,15 @@
-
-    const scriptURL = 'https://script.google.com/macros/s/AKfycbz8MRi19oqT6jwLGPXgRhmyD2S4HE-EMIhjpwrk1FvxFL9n-Esk87uhwGYHoGoDPjIi/exec'
-    const form = document.forms['submit-to-google-sheet']
-
-    form.addEventListener('submit', e => {
-        e.preventDefault()
-        fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-        .then(response => console.log('Success!', response))
+const scriptURL = 'https://script.google.com/macros/s/AKfycbz8MRi19oqT6jwLGPXgRhmyD2S4HE-EMIhjpwrk1FvxFL9n-Esk87uhwGYHoGoDPjIi/exec'
+const form = document.forms['submit-to-google-sheet']
+const msg = document.getElementById('msg')
+form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, {method: 'POST', body: new FormData(form)})
+        .then(response => {
+            msg.innerHTML = "Die Nachricht wurde erfolgreich gesendet"
+            setTimeout(function () {
+                msg.innerHTML = ""
+            }, 5000)
+            form.reset()
+        })
         .catch(error => console.error('Error!', error.message))
-    })
+})
