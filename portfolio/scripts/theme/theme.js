b5ec51d42397ca29537076c29b084bca39f8d684
kingler <janlucakingler@gmail.com>
2025-08-25 16:58:36 +0200

    refactor(.css) font size for section header + button for white-/ darkmode

--- portfolio/scripts/theme/theme.js
+++ portfolio/scripts/theme/theme.js
@@ -1,31 +1,19 @@
-const toggleBtn = document.getElementById('theme-toggle');
-const root = document.documentElement;
+let whiteMode = localStorage.getItem("whiteMode");
+const themeSwitch = document.querySelector('#theme-switch'); // ID beachten
 
-// Prüfen, ob User ein gespeichertes Theme hat
-if (localStorage.getItem('theme') === 'dark') {
-    root.classList.add('dark');
+const enableWhiteMode = () => {
+    document.body.classList.add("whiteMode");
+    localStorage.setItem("whiteMode", "active");
 }
 
-toggleBtn.addEventListener('click', () => {
-    root.classList.toggle('dark');
+const disableWhiteMode = () => {
+    document.body.classList.remove("whiteMode");
+    localStorage.removeItem("whiteMode");
+}
 
-    // Speicherung im LocalStorage
-    if (root.classList.contains('dark')) {
-        localStorage.setItem('theme', 'dark');
-    } else {
-        localStorage.setItem('theme', 'light');
-    }
-});
+if(whiteMode === "active") enableWhiteMode();
 
-
-toggleBtn.addEventListener('click', () => {
-    root.classList.toggle('dark');
-
-    if (root.classList.contains('dark')) {
-        localStorage.setItem('theme', 'dark');
-        toggleBtn.querySelector('img').src = "https://img.icons8.com/ios-filled/50/ffffff/sun--v1.png";
-    } else {
-        localStorage.setItem('theme', 'light');
-        toggleBtn.querySelector('img').src = "https://img.icons8.com/ios-filled/50/000000/moon-symbol.png";
-    }
+themeSwitch.addEventListener("click", () => {
+    whiteMode = localStorage.getItem("whiteMode");
+    whiteMode !== "active" ? enableWhiteMode() : disableWhiteMode();
 });
