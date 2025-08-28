a1709f08c8370f4ef884ad6d9bf8b4f3846cab8c
kingler <janlucakingler@gmail.com>
2025-08-28 09:21:09 +0200

    refactor() delete unused code + translate + reformat

--- portfolio/scripts/theme/theme.js
+++ portfolio/scripts/theme/theme.js
@@ -5,7 +5,6 @@ const enableWhiteMode = () => {
     document.body.classList.add("whiteMode");
     localStorage.setItem("whiteMode", "active");
 }
-
 const disableWhiteMode = () => {
     document.body.classList.remove("whiteMode");
     localStorage.removeItem("whiteMode");
@@ -17,3 +16,19 @@ themeSwitch.addEventListener("click", () => {
     whiteMode = localStorage.getItem("whiteMode");
     whiteMode !== "active" ? enableWhiteMode() : disableWhiteMode();
 });
+
+const burger = document.getElementById('burger');
+const navLinks = document.getElementById('nav-links');
+const links = navLinks.querySelectorAll('a');
+
+burger.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+    burger.classList.toggle('open');
+});
+
+links.forEach(link => {
+    link.addEventListener('click', () => {
+        navLinks.classList.remove('active');
+        burger.classList.remove('open');
+    });
+})
