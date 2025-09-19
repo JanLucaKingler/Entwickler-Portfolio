741cf6fa0b9aa1072c45eee68c7c428c16006449
kingler <janlucakingler@gmail.com>
2025-09-19 10:12:40 +0200

    fix(navbar) show active selection

--- portfolio/scripts/theme/header.js
+++ portfolio/scripts/theme/header.js
@@ -1,17 +1,15 @@
-let menuIcon = document.getElementById('#menu-icon');
-let navbar = document.getElementById('.navbar');
+let menuIcon = document.getElementById('menu-icon');
+let navbar = document.querySelector('.navbar');
 
 menuIcon.onclick = () => {
     menuIcon.classList.toggle('bx-x');
     navbar.classList.toggle('active');
 }
 
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
 
-
-let sections = document.querySelectorAll("section");
-let navLinks = document.querySelectorAll(" header nav a");
-
-window.onscroll = ()=> {
+window.onscroll = () => {
     sections.forEach(sec => {
         let top = window.scrollY;
         let offset = sec.offsetTop - 100;
@@ -19,13 +17,13 @@ window.onscroll = ()=> {
         let id = sec.getAttribute('id');
 
         if (top >= offset && top < offset + height) {
-            navLinks.forEach(links => {
-                links.classList.remove("active");
-                document.querySelector('header nav a[href*=' + id +']').classList.add('active');
-            })
+            navLinks.forEach(link => {
+                link.classList.remove('active');
+            document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
         }
-    })
-    let header = document.querySelector('header');
+    });
 
-    header.classList.toggle('sticky', window.scrollY > 100);
-}
+    let header = document.querySelector("header");
+    header.classList.toggle("sticky", window.scrollY > 100);
+};
